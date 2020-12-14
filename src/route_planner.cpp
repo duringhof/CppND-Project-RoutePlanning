30aca2bdce956b7ae155a0566669221890635389
Hans-Martin Duringhof <hmduringhof@gmail.com>
2020-12-14 08:01:39 +0100

    Minor fixes from Reviewer feedback

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -1,18 +1,22 @@
 #include "route_planner.h"
 #include <algorithm>
 
-RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y): m_Model(model) {
-    // Convert inputs to percentage:
-    start_x *= 0.01;
-    start_y *= 0.01;
-    end_x *= 0.01;
-    end_y *= 0.01;
+using std::reverse;
 
-    // Find the closest nodes to the starting and ending coordinates. Store the nodes in the
-    // RoutePlanner's start_node and end_node attributes.
-    start_node = &m_Model.FindClosestNode(start_x, start_y);
-    end_node = &m_Model.FindClosestNode(end_x, end_y);
-    }
+RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y,
+                           float end_x, float end_y)
+    : m_Model(model) {
+  // Convert inputs to percentage:
+  start_x *= 0.01;
+  start_y *= 0.01;
+  end_x *= 0.01;
+  end_y *= 0.01;
+
+  // Find the closest nodes to the starting and ending coordinates. Store the
+  // nodes in the RoutePlanner's start_node and end_node attributes.
+  start_node = &m_Model.FindClosestNode(start_x, start_y);
+  end_node = &m_Model.FindClosestNode(end_x, end_y);
+}
 
 // Calculate the H value.
 float RoutePlanner::CalculateHValue(RouteModel::Node const *node) {
@@ -35,8 +39,8 @@ void RoutePlanner::AddNeighbors(RouteModel::Node *current_node) {
 
 // Sort the open list and return the next node
 bool Compare(RouteModel::Node* a, RouteModel::Node* b) {
-    float f1 = a->g_value + a->h_value;
-    float f2 = b->g_value + b->h_value;
+    const float f1 = a->g_value + a->h_value;
+    const float f2 = b->g_value + b->h_value;
     return f1 > f2;
 }
 
@@ -55,10 +59,11 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
     RouteModel::Node* tempnode = current_node;
     while (tempnode != start_node) {
       distance = distance + tempnode->distance(*tempnode->parent);
-      path_found.insert(path_found.begin(), *tempnode);
+      path_found.insert(path_found.end(), *tempnode);
       tempnode = tempnode->parent;
     }
-    path_found.insert(path_found.begin(), *tempnode);
+    path_found.insert(path_found.end(), *tempnode);
+    reverse(path_found.begin(), path_found.end());
     distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
     return path_found;
 }
